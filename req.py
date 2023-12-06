d8fc02fa4b725014f001bf9e26a57b23cd9f90c6
Berkay Güler <berkayguler7@hotmail.com>
2023-12-06 16:24:54 +0300

    Create req.py

---
+++ req.py
@@ -0,0 +1,36 @@
+import requests
+import json
+from picamera import PiCamera
+import requests
+from time import sleep
+
+# Örnek bir resim dosyasının yolu
+image_path = './cam.jpg'
+
+# POST isteği için endpoint URL'si
+url = 'http://greenscan.pythonanywhere.com/capture'
+camera = PiCamera()
+camera.resolution = (1024, 768)
+camera.start_preview()
+sleep(2)
+#TODO: implement camera and send picture in post request to webservice
+def getCommand():
+    camera.capture('cam.jpg')
+    sleep(0.4)
+    with open(image_path, 'rb') as file:
+        files = {'image': file}
+        response = requests.post(url, files=files)
+    # API'nin yanıtını kontrol et
+    if response.status_code == 200:
+        try:
+            result = json.loads(response.text)
+            print("Etiket: ", result['label'])
+            first_char = str(result['label'][0])
+            chars = ['G', 'H', 'P', 'A']
+            for i in range(len(chars)):
+                if first_char == chars[i]:
+                    return i+1
+        except Exception as e:
+            print("Hata Oluştu: ",str(e))
+    else:
+        print("İstek başarısız. HTTP Hata Kodu:", response.status_code)

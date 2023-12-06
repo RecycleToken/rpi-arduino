bad9b70aa78c4ceb9af89c5c2129c126ec7160bb
Berkay Güler <berkayguler7@hotmail.com>
2023-12-06 16:23:57 +0300

    Update serial.py, rudimentary comments

--- serial.py
+++ serial.py
@@ -5,19 +5,19 @@ import req
 import web3client
 
 def process():
-    command = req.getCommand()
-    print("Command: {}".format(command))
-    ser.write("{}\n".format(command).encode('utf-8'))
-    commands = ["1","2","3","4"]
+    command = req.getCommand() #Gets command from the Image Processing API
+    print("Command: {}".format(command)) #DEBUG: Print command
+    ser.write("{}\n".format(command).encode('utf-8')) #Write the command to arduino through USB Serial
+    commands = ["1","2","3","4"] #Hardcoded commands
     if command in commands:
-        web3client.add_carbon(10)
-    time.sleep(3)
+        web3client.add_carbon(10) #TODO: Get web3 to work or change this to a request to web3 api
+    time.sleep(3) #Allowing time for motors
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) #Establish connection between RPI and Arduino
     time.sleep(3)
     while True:
-        arduino_status = ser.readline().decode('utf-8').rstrip()
+        arduino_status = ser.readline().decode('utf-8').rstrip() #Read line from arduino
         if arduino_status == "capture":
-            ser.flushInput()
-            process()
+            ser.flushInput() #Flush duplicates
+            process() #Do the thing

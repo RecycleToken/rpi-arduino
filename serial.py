5a9a58bb08590967384c575ca35d18f4745aa0c0
Berkay Güler <berkayguler7@hotmail.com>
2023-12-06 16:22:32 +0300

    web3 doesn't work here

--- serial.py
+++ serial.py
@@ -2,11 +2,15 @@
 import serial
 import time
 import req
+import web3client
 
 def process():
     command = req.getCommand()
     print("Command: {}".format(command))
     ser.write("{}\n".format(command).encode('utf-8'))
+    commands = ["1","2","3","4"]
+    if command in commands:
+        web3client.add_carbon(10)
     time.sleep(3)
 
 if __name__ == '__main__':
@@ -15,4 +19,5 @@ if __name__ == '__main__':
     while True:
         arduino_status = ser.readline().decode('utf-8').rstrip()
         if arduino_status == "capture":
+            ser.flushInput()
             process()

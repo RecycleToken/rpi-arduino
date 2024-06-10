1fecb8d5e4658948e888cb30abc4e027f27f5cdb
Berkay Güler <berkayguler7@hotmail.com>
2024-06-10 14:57:28 +0300

    Create queue_api.py

---
+++ queue_api.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+url = 'https://green-dashboard.mehmetalicakmak.org/api/'
+
+def get_queue():
+    response = requests.post(url + 'get-queue')
+    # print("Queue response: ", json.dumps(response.json(), indent=4)) 
+    return response
+
+def update_queue(data):
+    response = requests.post(url + 'update-queue', json=data)
+    # print("Update queue Data: ", json.dumps(data, indent=4))
+    # print("Queue update response: ", json.dumps(response.json(), indent=4))
+    return response
+
+# example queue data 
+# {
+#   "status":"Ready",
+#   "user": "666451049484d4c3e65fe43c"
+# }
+
+def create_transaction(data):
+    response = requests.post(url + 'create-transaction', json=data)
+    print("Transaction Data: ", json.dumps(data, indent=4))
+    print("Transaction response: ", json.dumps(response.json(), indent=4))
+    return response
+
+# example transaction data 
+#{
+#   "recycled": {
+#     "plastic": 10,
+#     "glass": 50,
+#     "metal": 30,
+#     "paper": 20
+#   },
+#   "user": "666451049484d4c3e65fe43c"
+# }
